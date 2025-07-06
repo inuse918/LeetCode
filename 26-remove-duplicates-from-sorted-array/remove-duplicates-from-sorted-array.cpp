ac3f96a11f76a682076e49097fbdbb955a98c3a0
Chaerin Yu <yuc2918@gmail.com>
2025-07-06 22:11:59 +0900

    Time: 0 ms (100.00%) | Memory: 22.6 MB (78.49%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int ptr = 0;
+        int ptr = 0; // ptr 은 중복 안된 고유 값 나왔을 때 이동될 nums index
 
         for (int i = 1 ; i< nums.size(); i++){
-            if (nums[ptr] != nums[i]){
+            /* [0,0,1,1,1,2,2,3,3,4] 에서
+               nums[0] != nums[2] :
+                nums [1] = nums[2] -> 1번 index에 nums[2] = 1 대입
+            */
+            if (nums[ptr] != nums[i]){  
                 nums [++ptr] = nums[i];
             }
         }
-        return ptr+1;
+        return ptr+1; // index 번호이므로 +1 return
     }
 };
