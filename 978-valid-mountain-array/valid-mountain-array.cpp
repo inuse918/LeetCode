0ae82e4bba0b63fb0f8b181edbc5abb0c9ad632f
Chaerin Yu <yuc2918@gmail.com>
2025-07-10 00:04:34 +0900

    Time: 18 ms (69.38%) | Memory: 26.3 MB (8.52%) - LeetSync

--- 978-valid-mountain-array/valid-mountain-array.cpp
+++ 978-valid-mountain-array/valid-mountain-array.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
-        int n = arr.size(), i = 0, j = n - 1;
-        while (i + 1 < n && arr[i] < arr[i + 1]) i++;
-        while (j > 0 && arr[j - 1] > arr[j]) j--;
-        return i > 0 && i == j && j < n - 1;
+        int n = arr.size();
+        if(n < 3) return false; // 길이 3 미만은 해당 X 
+        int i = 1;
+
+        // i가 arr.size까지 오르막인경우 i++
+        while(i < n && arr[i] > arr[i-1]) i++; 
+        // i가 1(오르막 없음) || i == arr.size면 return false;
+        if(i == 1 || i == n) return false; 
+        // i가 정점에서 arr.size까지 내리막인경우 i ++
+        while(i < n && arr[i] < arr[i-1]) i++;
+        
+        // i 가 arr.size까지 내리막 유지 하면 true 리턴
+        return (i==n);
     }
 };
