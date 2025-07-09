5e0a3b0cfc4fad88cff1434fb1cb19c6b1db4e02
Chaerin Yu <yuc2918@gmail.com>
2025-07-10 00:08:15 +0900

    Time: 15 ms (88.38%) | Memory: 26.1 MB (71.71%) - LeetSync

--- 978-valid-mountain-array/valid-mountain-array.cpp
+++ 978-valid-mountain-array/valid-mountain-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
-        int n = arr.size();
+        /*int n = arr.size();
         if(n < 3) return false; // 길이 3 미만은 해당 X 
         int i = 1;
 
@@ -13,6 +13,27 @@ public:
         while(i < n && arr[i] < arr[i-1]) i++;
         
         // i 가 arr.size까지 내리막 유지 하면 true 리턴
-        return (i==n);
+        return (i==n);*/
+
+        
+
+        if(arr.size() < 3 
+            || arr[0] > arr[1] 
+            || arr[arr.size()-1] > arr[arr.size()-2]) {
+            return false;
+        }
+        int i=0;
+        for(;i<arr.size()-1;i++){    
+            if(arr[i] >= arr[i+1]){ // 내림차순 마주하면 break. 여기서 i는 정점
+                break;
+            }
+        }
+
+        for(;i<arr.size()-1;i++){
+            if(arr[i] <= arr[i+1]){ // 정점에서 오르막길 다시 발견하면 false
+                return false;
+            }
+        }
+        return true;
     }
 };
