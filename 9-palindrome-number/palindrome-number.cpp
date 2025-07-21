f4620f535eeed81a5347ed3f745dbc2b826ec73a
Chaerin Yu <yuc2918@gmail.com>
2025-07-21 21:14:53 +0900

    Time: 0 ms (100.00%) | Memory: 8.6 MB (41.46%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string str = to_string(x);
-        string compareStr = str; 
-        reverse(compareStr.begin(), compareStr.end());
+        // 방법1 : 내가 작성한 코드
+        // string str = to_string(x);
+        // string compareStr = str; 
+        // reverse(compareStr.begin(), compareStr.end());
 
-        for(int i =0 ; i < str.length() ; i++) {
-            if (str[i] != compareStr[i]) { return false; }
+        // for(int i =0 ; i < str.length() ; i++) {
+        //     if (str[i] != compareStr[i]) { return false; }
+        // }
+        // return true;
+
+        // 방법2
+
+        if ( x < 0 ) return false ; // 음수는 무조건 false
+
+        long reverse = 0;
+        int x2 = x;
+
+        while (x > 0){
+            reverse = (reverse * 10 ) + (x % 10);
+            x /= 10; 
         }
-        return true;
+
+        return reverse == x2;
+
     }
 };
