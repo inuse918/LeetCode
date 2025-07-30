3cf5323ac073195b6e8409217ecf7799d8c599b8
Chaerin Yu <yuc2918@gmail.com>
2025-07-30 17:23:16 +0900

    Time: 0 ms (100.00%) | Memory: 8.7 MB (90.64%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -3,20 +3,27 @@ public:
     int lengthOfLastWord(string s) {
         int startPtr = 0, endPtr = s.length() -1; 
 
-        while (endPtr >= 0 ) {
+        /* while (endPtr >= 0 ) {
             if (s[endPtr] == ' ') {
                 endPtr--;
                 continue;
             } else {
                 break;
             }
+        }*/
+        while (endPtr >= 0 && s[endPtr] == ' ') {
+            endPtr--;
         }
+
         startPtr = endPtr;
-        while (startPtr >=0){
+        /* while (startPtr >=0){
             if (s[startPtr] == ' '){
                 break;
             }
             startPtr --;
+        }*/
+         while (startPtr >= 0 && s[startPtr] != ' ') {
+            startPtr--;
         }
         return endPtr - startPtr; 
     }
