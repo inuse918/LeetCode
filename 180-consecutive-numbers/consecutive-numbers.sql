ad711906dd8f1f2dd0eeea9c3b80ab9faeac7f4c
Chaerin Yu <yuc2918@gmail.com>
2025-08-06 19:20:03 +0900

    Time: 1824 ms (5.05%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,4 +1,4 @@
-select distinct num as "ConsecutiveNums"
+/*select distinct num as "ConsecutiveNums"
 from (
         SELECT num
             , LAG(num) OVER (ORDER BY id) AS prev_num -- 현재 행 기준 이전행의 num 가져옴 
@@ -6,4 +6,12 @@ from (
         FROM Logs 
     ) 
 WHERE num = prev_num AND num = next_num;
-    ;
+    ;*/
+-- 2번째 방법... 
+SELECT DISTINCT  l1.num AS ConsecutiveNums
+FROM   Logs l1, Logs l2, Logs l3
+WHERE  l1.num = l2.num
+AND    l2.num = l3.num
+ -- join 방법~~
+AND    l1.id = l2.id-1
+AND    l2.id = l3.id-1
