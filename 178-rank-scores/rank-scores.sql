6ea08e4e08013fccbc242c0c0256e69e73416246
Chaerin Yu <yuc2918@gmail.com>
2025-08-11 17:11:11 +0900

    Time: 404 ms (45.27%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,5 +1,13 @@
-SELECT score,
+/*SELECT score,
        DENSE_RANK() OVER (ORDER BY score DESC) AS rank-- 순위 매기는 함수 
     
 FROM   scores
-ORDER BY score DESC;
+ORDER BY score DESC; */
+
+select score
+    , (select count(distinct b.score)
+        from Scores b
+        where b.score >= a.score ) as "rank" -- a의 score보다 큰 점수 count, 즉 1등 점수가 두 번 있어도 그 다음
+        -- 등수가 바로 2등이 됨
+from   scores a
+order by a.score desc;
