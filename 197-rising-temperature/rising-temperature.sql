bfb09f354902523eaef0772c58807a7ce2abc18c
Chaerin Yu <yuc2918@gmail.com>
2025-08-12 20:20:41 +0900

    Time: 662 ms (19.53%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,6 +1,13 @@
-/* Write your PL/SQL query statement below */
+/*
 select b.id 
 from   Weather a
      , Weather b
 where  to_char(to_date(a.recordDate +1),'yyyymmdd') = b.recordDate 
 and    a.temperature < b.temperature
+;;*/
+
+SELECT today.id 
+FROM Weather yesterday
+CROSS JOIN Weather today -- 카티션 곱
+WHERE today.recorddate - yesterday.recorddate = 1 -- 하루 차이만 남김 
+  AND today.temperature > yesterday.temperature;
